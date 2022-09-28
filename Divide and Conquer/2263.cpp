21a6d537d469cf66dd45f31bd417fd31b90b2ec1
3seoksw <3suksw@gmail.com>
2022-09-28 21:55:17 +0900

    2263 upload

--- Divide and Conquer/2263.cpp
+++ Divide and Conquer/2263.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int inOrder[100000];
-int postOrder[100000];
+/*
+ * InOrder: Left>Root>Right
+ * PreOrder: Root>Left>Right 
+ * PostOrder: Left>Right>Root
+*/
+
+int inOrder[100001];
+int postOrder[100001];
+int idxArr[100001];
+
+void preOrder(int inStart, int inEnd, int postStart, int postEnd)
+{
+	if (inStart > inEnd || postStart > postEnd)
+		return;
+
+	int root = postOrder[postEnd];
+	int rootIdx = idxArr[root];
+	int leftSize = rootIdx - inStart;
+	int rightSize = inEnd - rootIdx;
+	
+	cout << inOrder[rootIdx] << ' ';
+
+	preOrder(inStart, rootIdx - 1, postStart, postStart + leftSize - 1);
+	preOrder(rootIdx + 1, inEnd, postStart + leftSize, postEnd - 1);
+}
+
 int main(void)
 {
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> 
-		}
+	for (int i = 1; i <= n; i++) {
+		cin >> inOrder[i];
+		idxArr[inOrder[i]] = i;
 	}
+
+	for (int i = 1; i <= n; i++)
+		cin >> postOrder[i];
+
+	preOrder(1, n, 1, n);
+	cout << "\n";
+
 	return 0;
 }
