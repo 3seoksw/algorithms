8cb08d4f95628ceafcd328336e0217d38c14fe08
3seoksw <3suksw@gmail.com>
2022-09-29 18:06:12 +0900

    uploading algorithm pe02

--- Divide and Conquer/10830.cpp
+++ Divide and Conquer/10830.cpp
@@ -1,17 +1,59 @@
 #include <iostream>
 using namespace std;
 
-int matrix[5][5];
+long long N, B;
+long long matrix[5][5];
+long long temp[5][5];
+long long result[5][5];
+
+void multiMatrix(long long arr1[5][5], long long arr2[5][5])
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			temp[i][j] = 0;
+			for (int k = 0; k < N; k++) {
+				temp[i][j] += arr1[i][k] * arr2[k][j];
+			}
+			temp[i][j] %= 1000;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr1[i][j] = temp[i][j];
+		}
+	}
+}
+
+void printMatrix(int N)
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << result[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 
 int main(void)
 {
-	int N, B;
 	cin >> N >> B;
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> matrix[i][j];
 		}
+		result[i][i] = 1;
 	}
+
+	while (B > 0) {
+		if (B % 2 == 1) {
+			multiMatrix(result, matrix);
+		}
+		multiMatrix(matrix, matrix);
+		B /= 2;
+	}
+	printMatrix(N);
+
 	return 0;
 }
--- Divide and Conquer/1725.cpp
+++ Divide and Conquer/1725.cpp
@@ -2,25 +2,23 @@
 #include <algorithm>
 using namespace std;
 
-int n;
 int heights[100000];
 
-int histogram(int start, int end)
+int histogram(int start, int end) 
 {
 	if (start == end) {
 		return heights[start];
 	}
-	
+
 	int mid = (start + end) / 2;
 	int result = max(histogram(start, mid), histogram(mid + 1, end));
-
 	int l = mid;
 	int r = mid;
 	int temp = heights[mid];
 
 	while (l > start || r < end) {
-		int moveL = l > start ? heights[l - 1]: -1;
-		int moveR = r < end ? heights[r + 1]: -1;
+		int moveL = l > start ? heights[l - 1] : -1;
+		int moveR = r < end ? heights[r + 1] : -1;
 
 		if (moveL > moveR) {
 			temp = min(temp, moveL);
@@ -37,13 +35,14 @@ int histogram(int start, int end)
 
 int main(void)
 {
-	do {
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			cin >> heights[i];
-		}
-		cout << histogram(0, n - 1) << endl;
-	} while (n != 0); 
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> heights[i];
+	}
+
+	cout << histogram(0, N - 1) << endl;
 
 	return 0;
 }
